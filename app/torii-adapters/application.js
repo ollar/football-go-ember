d82f1bc17503685b2cb11b8ddeeddbbccdf6fedb
Ларькин Олег Иванович <o.larkin@bssys.com>
2017-06-07 18:36:44 +0300

    annoing message removed

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -2,18 +2,10 @@ import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
 import Ember from 'ember';
 
 export default ToriiFirebaseAdapter.extend({
-  // register() {
-  //   return this.get('firebaseApp').auth().createUserWithEmailAndPassword()
-  //   .catch(function(error) {
-  //     Ember.getOwner(this).send('error', error);
-  //   });
-  // },
-  init() {
-    this._super(...arguments);
-
-    // console.log('aaa', this);
+  register() {
+    return this.get('firebaseApp').auth().createUserWithEmailAndPassword()
+    .catch(function(error) {
+      Ember.getOwner(this).send('error', error);
+    });
   },
-  ololo() {
-    return '4';
-  }
 });
