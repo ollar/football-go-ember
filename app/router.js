5a030e9dac60fbc20f207e60f7f54697a74ef866
Oleg Larkin <olegollar@gmail.com>
2017-07-28 00:05:08 +0300

    profile dev

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('login');
   this.route('contacts');
   this.route('register');
+  this.route('profile', { path: 'profile/:player_id'});
 });
 
 export default Router;
